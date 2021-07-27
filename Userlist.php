9bcf7bc6ed0c77061eeaeed144ed65871158e3c4
yasmin-soltana <yasminfeni1998@gmail.com>
2021-07-27 11:04:40 +0600

    first commit

---
+++ Userlist.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>User list</title>
+</head>
+<body>
+  <h1>User list</h1>
+  <form action=" ajax/Userlist.php" method ="GET">
+  	<input type="text" name="1" value ="1"><br><br>
+  	<input type="text" name="2" value ="2"><br><br>
+  	<input type="text" name="3" value ="3"><br><br>
+  	<input type="text" name="4" value ="4"><br><br>
+  	<input type="submit" name="search" value = "search">
+  </form>
+   <hr>
+   
+   	<span style = "color:green"></span>
+   	<span style = "color:red"></span>
+  <br><br>
+  <a href ="logout.php">logout</a>
+</body>
+</html>
